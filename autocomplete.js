76763f5cf139284828d569e36cb83cc9e7e1589a
5zul <136836444+5zul4not4@users.noreply.github.com>
2023-08-08 13:32:05 +0530

    Add files via upload

---
+++ autocomplete.js
@@ -0,0 +1,53 @@
+let availableKeywords=[
+    'Vandalore',
+    'Chrompet',
+    'Chennai',
+    'Pondicherry',
+    'Cuddalore',
+    'Medavakkam',
+    'Pallavaram',
+    'Tambaram',
+    'T.Nagar',
+    'Chengalpet',
+    'Ottery',
+    'acharapakkam',
+    'Guindy',
+    'Guduvanchery',
+    'Adayar',
+    'Mamallapuram',
+    'Madipakkam'
+
+];
+
+const resultBox=document.querySelector(".result-box");
+const inputBox =document.getElementById("input-box");
+
+inputBox.onkeyup=function(){
+    let result=[];
+    let input=inputBox.value;
+    if(input.length){
+        result=availableKeywords.filter((keyword)=>{
+        return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result);
+    }
+
+    display(result);
+
+    if(!result.length){
+        resultBox.innerHTML='No result';
+    }
+}
+
+function display(result){
+    const content=result.map((list)=>{
+        return "<li onclick=selectInput(this)>" + list + "</li>";
+    });
+
+    resultBox.innerHTML="<ul>" + content.join('') + "</ul>";
+}
+
+function selectInput(list){
+    inputBox.value=list.innerHTML;
+    resultBox.innerHTML='';
+}
