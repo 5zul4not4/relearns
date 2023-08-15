b9687c9f6f2c6adb803e47182a9b8a193e748df2
5zul <136836444+5zul4not4@users.noreply.github.com>
2023-08-15 17:36:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var tabLinks = document.getElementsByClassName('tab-links');
-        var tabContents = document.getElementsByClassName('tab-contents');
+var tabContents = document.getElementsByClassName('tab-contents');
+const navbar= document.querySelector('.navbar');
 
         function openTab(tabName) {
             for (tabLink of tabLinks) {
@@ -12,11 +13,156 @@ var tabLinks = document.getElementsByClassName('tab-links');
             document.getElementById(tabName).classList.add('active-tab');
         }
 
-        // JavaScript for handling the mobile navigation menu
         document.querySelector('.fas.fa-bars').addEventListener('click', function () {
-            document.querySelector('nav ul').classList.toggle('show');
-        });
+        document.querySelector('nav ul').classList.toggle('show');
+      });
 
-        document.querySelector('.fas.fa-times').addEventListener('click', function () {
-            document.querySelector('nav ul').classList.remove('show');
-        });
+      document.addEventListener('click', (event) => {
+          if(!navbar.contains(event.target)){
+navbar.classList.remove('show');
+document.querySelector('nav ul').classList.remove('show');
+          }
+      });
+
+document.querySelector('.fas.fa-times').addEventListener('click', function () {
+          document.querySelector('nav ul').classList.remove('show');
+      })
+    
+const pass=document.querySelector("#password");
+const pass2=document.querySelector("#confirmPassword");
+const checkbox=document.querySelector("#check");
+function switchVisibility(){
+if(pass.getAttribute("type")==="password")
+  pass.setAttribute("type","text");
+else
+  pass.setAttribute("type","password");
+}
+
+function checkpass(){
+  if(pass.value===pass2.value){
+  }
+  else
+  {
+  checkbox.checked=false;
+  alert('Password does not match!');
+  }
+}
+
+
+  document.querySelector('form').addEventListener('submit', function(event) {
+    var fileInput = document.getElementById('fileInput');
+    var fileSize = fileInput.files[0].size; // Get the file size in bytes
+    var maxSize = 1048576; // Define the maximum allowed file size in bytes (e.g., 1MB)
+    var age = document.getElementById('age');
+    if (fileSize > maxSize) {
+      event.preventDefault(); // Prevent form submission
+      alert('File size exceeds the limit. Please choose a smaller file.');
+    }
+     else if(pass.value!==pass2.value){
+      event.preventDefault();
+      alert("Password doesn't match!");
+     }
+     else if(age.value<18){
+      event.preventDefault();
+      alert("Your age is not qualified!");
+      }
+     else
+      document.getElementById('myForm').submit();
+  });
+
+function openTermsAndConditions(event) {
+event.preventDefault();
+document.getElementById('termsModal').style.display = 'block';
+}
+
+function acceptTerms() {
+document.getElementById('termsModal').style.display = 'none';
+document.getElementById('myForm').submit();
+}
+
+function declineTerms() {
+document.getElementById('termsModal').style.display = 'none';
+alert('You must accept the terms and conditions to submit the form.');
+}
+
+function termsAndConditions(event) {
+event.preventDefault();
+document.getElementById('termstyle').style.display = 'block';
+}
+function readTerms() {
+document.getElementById('termstyle').style.display = 'none';
+}
+
+
+/*
+  document.getElementById("registrationForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent the form from submitting normally
+
+  
+  // Get form values
+  var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastname").value;
+  var gender = document.getElementById("gender").value;
+  var age = document.getElementById("age").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("email").value;
+  var centerName = document.getElementById("centername").value;
+  var centerAddress = document.getElementById("centeraddress").value;
+  var qualification = document.getElementById("qualification").value;
+  var experience = document.getElementById("experience").value;
+  var interest = document.getElementById("interest").value;
+  var subject = document.getElementById("subject").value;
+  var mode = document.getElementById("mode").value;
+  var map = document.getElementById("address").value;
+
+
+
+
+  // Create a new window to print the details
+  var printWindow = window.open('','Form');
+  // Generate the content for printing
+  var content = "<h2>Registration Details</h2>" +
+                "<strong>Tutor Name:</strong> " + firstName +" " +lastName+
+                "<br><strong>Gender:</strong> " + gender +
+                "<br><strong>Age:</strong> " + age +
+                "<br><strong>Contact:</strong> " + phone +
+                "<br><strong>Email:</strong> " + email +
+                "<br><strong>Tuition Center:</strong> " + centerName +
+                "<br><strong>Tuition Address:</strong> " + centerAddress +
+                "<br><strong>Education Qualification : </strong> " + qualification +
+                "<br><strong>Class:</strong> " + interest +
+                "<br><strong>Subject:</strong> " + subject +
+                "<br><strong><input type='button' onclick='window.print()' value='Print'></strong> "
+
+                // Add other fields' values in a similar manner
+
+  // Set the content and print the window
+  printWindow.document.open();
+  printWindow.document.write(content);
+  printWindow.document.close();
+  
+
+  // Reset the form
+  document.getElementById("registrationForm");
+});*/
+
+function changeInputType() {
+  var input = document.getElementById("contactInput");
+  var value = input.value;
+
+  // Checking if the value matches a phone number format
+  var phoneRegex = /^[0-9()+-]*$/;
+  if (phoneRegex.test(value)) {
+    input.type = "tel";
+    input.maxSize="10";
+  }
+  // Checking if the value matches an email format
+  else if (value.includes("@")) {
+    input.type = "email";
+    
+  }
+  // If the value doesn't match any format, default to text
+  else {
+    input.type = "text";
+  }
+}
